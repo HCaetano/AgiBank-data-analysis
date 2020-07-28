a7680673c7165c71068db925f59e9093a7ce7a65
Havyner Caetano <hrcgeo@gmail.com>
2020-07-28 12:29:42 -0300

    Add read and write file, counts number of salespeople and customers

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,36 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> lines = new ArrayList<>();
-
+        final ArrayList<String> fileContents = new ArrayList<>();
+        final ArrayList<String> report = new ArrayList<>();
+        Integer numberOfCustomers = 0;
+        Integer numberOfSalespeople = 0;
+        Integer biggestSaleId = 0;
+        Integer lowestRankingSalesperson = 0;
+        String reportNumberPath = System.getProperty("user.dir") + "/src/resources/reportNumber.dat";
+        String reportNumber = "1";
+        File data = null;
+        
         try {
-            File file = new File(System.getProperty("user.home") + "/data/in/file1.dat");
-            Scanner scanner = new Scanner(file);
+            data = new File(System.getProperty("user.home") + "/data/in/file1.dat");
+            Scanner scanner = new Scanner(data);
 
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                lines.add(line);
+                fileContents.add(line);
+            }
+
+            File reportNumberInfo = new File(reportNumberPath);
+            scanner = new Scanner(reportNumberInfo);
+
+            while (scanner.hasNextLine()) {
+                reportNumber = scanner.nextLine();
             }
 
             scanner.close();
@@ -25,10 +41,50 @@ public class Main {
             e.printStackTrace();
         }
 
-        Path out = Paths.get(System.getProperty("user.home") + "/data/out/file_out.dat");
+        for (String line : fileContents) {
+            if (line.substring(0, 3).equals("001")) {
+                numberOfSalespeople++;
+            } else if (line.substring(0, 3).equals("002")) {
+                numberOfCustomers++;
+            }
+        }
+
+        Integer updatedReportNumber = Integer.parseInt(reportNumber);
+        updatedReportNumber++;
+        reportNumber = updatedReportNumber.toString();
+        report.add("*** Report " + reportNumber + " ***");
+
+        for (int reportIndicator = 0; reportIndicator < 4; reportIndicator++) {
+            switch (reportIndicator) {
+                case 1:
+                    report.add("Number of clients: " + numberOfCustomers);
+                case 2:
+                    report.add("Number of salespeople: " + numberOfSalespeople);
+                case 3:
+                    report.add("Biggest sale ID: " + biggestSaleId);
+                case 4:
+                    report.add("Salesperson with lowest ranking sale : " + lowestRankingSalesperson);
+            }
+        }
+
+        report.add("*** End report ***");
+        report.add("\n");
+
+        Path reportDestination = Paths.get(System.getProperty("user.home") + "/data/out/report.dat");
+        Path reportNumberDestination = Paths.get(reportNumberPath);
 
         try {
-            Files.write(out, lines, Charset.defaultCharset());
+            String reportNumberString = reportNumber;
+            byte[] stringToBytes = reportNumberString.getBytes();
+
+            Files.write(reportNumberDestination, stringToBytes);
+
+            if (data.length() == 0) {
+                Files.write(reportDestination, report, Charset.defaultCharset());
+            } else {
+                String reportInOneString = String.join("\n", report );
+                Files.write(reportDestination, reportInOneString.getBytes(), StandardOpenOption.APPEND);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
