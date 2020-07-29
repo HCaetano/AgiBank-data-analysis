d8cb96669f3dddf2023218ad035ba13e5dae41ff
Havyner Caetano <hrcgeo@gmail.com>
2020-07-29 10:43:35 -0300

    Create Salesperson entity, remove its how many functionality from main

---
+++ src/Entity/Salesperson.java
@@ -0,0 +1,24 @@
+package Entity;
+
+public class Salesperson {
+
+    private String name;
+    private String cpf;
+    private Double salary;
+
+    public Salesperson() {
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+}
