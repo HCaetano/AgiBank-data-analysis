e099bcc618c8044a697f327b174424827ba65f06
Havyner Caetano <hrcgeo@gmail.com>
2020-07-29 19:21:52 -0300

    Create Analyst class to move logic from Main to it

---
+++ src/Analysis/Analyst.java
@@ -0,0 +1,54 @@
+package Analysis;
+
+import Entity.Item;
+import Entity.Report;
+import Entity.Sale;
+
+import java.util.ArrayList;
+
+public class Analyst {
+
+    public Analyst() {
+
+    }
+
+    public ArrayList<Item> processMultipleItems(String processedLine) {
+        String[] itemsContent = processedLine.split(",");
+        ArrayList<Item> items = new ArrayList<>();
+
+        for (String itemContent : itemsContent) {
+            String itemsInfo[] = itemContent.split("-");
+            Item item = new Item();
+
+            item.setId(Integer.parseInt(itemsInfo[0]));
+            item.setQuantity(Integer.parseInt(itemsInfo[1]));
+            item.setPrice(Double.parseDouble(itemsInfo[2]));
+            items.add(item);
+        }
+
+        return items;
+    }
+
+    public ArrayList<Item> processSingleItem(String processedLine) {
+        String itemsInfo[] = processedLine.split("-");
+        ArrayList<Item> items = new ArrayList<>();
+        Item item = new Item();
+
+        item.setId(Integer.parseInt(itemsInfo[0]));
+        item.setQuantity(Integer.parseInt(itemsInfo[1]));
+        item.setPrice(Double.parseDouble(itemsInfo[2]));
+        items.add(item);
+
+        return items;
+    }
+
+    public double processSaleValue(ArrayList<Item> items) {
+        double saleValue = 0.0;
+
+        for (Item item : items) {
+            saleValue += item.getQuantity() * item.getPrice();
+        }
+
+        return saleValue;
+    }
+}
