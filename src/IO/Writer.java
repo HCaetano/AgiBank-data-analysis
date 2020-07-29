5cd260ff14831fd139504de2c0c63367583e5bc4
Havyner Caetano <hrcgeo@gmail.com>
2020-07-29 17:17:13 -0300

    Refactor Writer, removing report building from its responsibilities

--- src/IO/Writer.java
+++ src/IO/Writer.java
@@ -1,11 +1,11 @@
 package IO;
 
+import Entity.Report;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
 
 public class Writer {
 
@@ -13,35 +13,13 @@ public class Writer {
 
     }
 
-    public void writeReport(String reportNumber, ArrayList<String> report, int customersSize, int salesPeopleSize,
-                            String biggestSaleId, String lowestRankingSalespersonName) {
-        report.add("*** Report " + reportNumber + " ***");
-
-        for (int reportIndicator = 0; reportIndicator < 4; reportIndicator++) {
-            switch (reportIndicator) {
-                case 0:
-                    report.add("Number of customers: " + customersSize);
-                    break;
-                case 1:
-                    report.add("Number of salespeople: " + salesPeopleSize);
-                    break;
-                case 2:
-                    report.add("Biggest sale ID: " + biggestSaleId);
-                    break;
-                case 3:
-                    report.add("Salesperson with lowest ranking sale: " + lowestRankingSalespersonName);
-            }
-        }
-
-        report.add("*** End report ***");
-        report.add("\n");
-
+    public void writeReport(String reportNumber, Report report) {
         Path reportDestination = Paths.get(
                 System.getProperty("user.home") + "/data/out/report_" + reportNumber + ".done.dat"
         );
 
         try {
-            String reportInOneString = String.join("\n", report);
+            String reportInOneString = String.join("\n", report.toString());
             Files.write(reportDestination, reportInOneString.getBytes(), StandardOpenOption.CREATE);
         } catch (IOException e) {
             e.printStackTrace();
